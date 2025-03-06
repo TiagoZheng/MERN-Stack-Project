062a5ab2beea2da98d7d426b1f157ffb3556c956
Tiago Zheng <tiago.zheng23@gmail.com>
2025-03-06 20:20:57 +0000

    Minor tweaks and code cleanup

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ import productRoutes from "./routes/product.routes.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to access JSON data in the request body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000/");
+  console.log("Server started at http://localhost:" + PORT);
 });
