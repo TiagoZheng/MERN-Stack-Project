722bdcbec30bb01746d38629b92b486d393b5df4
Tiago Zheng <tiago.zheng23@gmail.com>
2025-03-06 20:11:25 +0000

    get all products and update products from db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+
+import productRoutes from "./routes/product.routes.js";
 
 dotenv.config();
 
@@ -9,32 +10,7 @@ const app = express();
 
 app.use(express.json()); // allows us to access JSON data in the request body
 
-app.post("/api/products", async (req, res) => {
-  const product = req.body; // user will send this data
-
-  if (!product.name || !product.price || !product.image) {
-    return res.status(400).json({ success: false, message: "Please provide all fields" });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("Error in Create Product", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: "Product deleted successfully" });
-  } catch (error) {}
-});
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
